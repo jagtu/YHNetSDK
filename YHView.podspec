0e310e2ee859b28839930b06706f3c3a17d29da2
oid <42318168@qq.com>
2018-03-21 10:26:20 +0800

    add file

---
+++ YHView.podspec
@@ -0,0 +1,27 @@
+
+Pod::Spec.new do |s|
+
+  s.name         = "YHView"
+  s.version      = "0.0.1"
+  s.summary      = "this is a test."
+
+  s.description  = "this is a test sdk this is a test dsdk"
+
+  s.homepage     = "https://github.com/jagtu/YHView.git"
+
+
+  s.license      = { :type => "MIT", :file => "FILE_LICENSE" }
+
+  s.author             = { "oid" => "42318168@qq.com" }
+
+  s.platform     = :ios, "8.0"
+  s.source       = { :git => "http://EXAMPLE/YHView.git", :tag => "0.0.1" }
+
+
+  s.source_files  = "Classes", "Classes/**/*.{h,m}"
+
+  s.requires_arc = true
+
+  s.vendored_frameworks = [‘Classes/YHThemeKit.framework’]
+
+end
---
+++ classes/YHThemeKit.framework/Headers/YHThemeKit.h
@@ -0,0 +1,32 @@
+//
+//  YHThemeKit.h
+//  YHThemeKit
+//
+//  Created by Jagtu on 2018/3/20.
+//  Copyright © 2018年 yh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YHThemeKit.
+FOUNDATION_EXPORT double YHThemeKitVersionNumber;
+
+//! Project version string for YHThemeKit.
+FOUNDATION_EXPORT const unsigned char YHThemeKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YHThemeKit/PublicHeader.h>
+
+
+#import <YHThemeKit/YHThemeManager.h>
+#import <YHThemeKit/YHThemeProperties.h>
+
+#import <YHThemeKit/NSObject+Theme.h>
+#import <YHThemeKit/UIView+Theme.h>
+#import <YHThemeKit/UILabel+Theme.h>
+#import <YHThemeKit/UIButton+Theme.h>
+#import <YHThemeKit/UIImageView+Theme.h>
+#import <YHThemeKit/UINavigationBar+Theme.h>
+#import <YHThemeKit/CALayer+Theme.h>
+#import <YHThemeKit/UIColor+Theme.h>
+#import <YHThemeKit/UIImage+Theme.h>
+
